99dceb46fb384e42f384efe165aae73f93cc6cff
Xavier Dutoit <xavier@tttp.eu>
2013-05-06 10:48:06 +0200

    empty framework

---
+++ js/field.js
@@ -0,0 +1,11 @@
+<script>
+cj (function($) {
+  if ($("#first_name").val() != "") {
+    $("#first_name").attr("readonly", true).css("border", 0).css("background-color", "inherit");
+  }
+
+  if ($("#last_name").val() != "") {
+    $("#last_name").attr("readonly", true).css("border", 0).css("background-color", "inherit");
+  }
+})(jQuery);
+</script>

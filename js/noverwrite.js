f5f4d480862f82ae678181564ed72dc86ba1ab93
Xavier Dutoit <xavier@tttp.eu>
2013-05-06 17:15:43 +0200

    with readme and js cleanup

--- js/noverwrite.js
+++ js/noverwrite.js
@@ -1,9 +1,7 @@
 cj(function($) {
-  if ($("#first_name").val() != "") {
-    $("#first_name").attr("readonly", true).css("border", 0).css("background-color", "inherit");
-  }
-
-  if ($("#last_name").val() != "") {
-    $("#last_name").attr("readonly", true).css("border", 0).css("background-color", "inherit");
-  }
+  $.each(["#first_name","#last_name"],function (k,v) {
+    if ($(v).val() != "") {
+      $(v).attr("readonly", true).css("border", 0).css("background-color", "inherit");
+    }
+  });
 });
